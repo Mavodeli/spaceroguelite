0ec05e167cf8d4f8d280323a6c92bc54e0a26880
EmptyN8 <marcel.eutingen@hotmail.com>
2023-01-04 12:31:45 +0100

    Added AnglerFishData
    Added AnglerFish Data so all Enemys have the same structure.
    Cleared up AnglerFishBehaviour.

--- Crabasite/Assets/Scripts/Enemies/AnglerFishBehaviour.cs
+++ Crabasite/Assets/Scripts/Enemies/AnglerFishBehaviour.cs
@@ -7,12 +7,12 @@ public class AnglerFishBehaviour : Enemy
     public float maxReachAttraction = 7f; // defines the Reach Value of the Attraction
     public float maxReachDamage = 3f; // defines the Range in which the enemy starts to deal damage
     public float AttractionForce = 50f; // defines the Force Value of the Attraction
-    
-    public float chaseSpeed = 5f;
+    private AnglerFishData afd;
 
     void Awake()//use this instead of Start(), bc Enemy.cs already uses Start()!
     {
-        Texture2D _sprite = Resources.Load<Texture2D>("Sprites/AnglerFish_256x256_New3");
+        afd = Resources.Load<AnglerFishData>("Scriptable Objects/EnemyData/AnglerFishData");
+        Texture2D _sprite = Resources.Load<Texture2D>(afd.texturePath);
         Sprite sprite = Sprite.Create(_sprite, //texture
                                         new Rect(0.0f, 0.0f, _sprite.width, _sprite.height), //subpart of the texture to create the sprite from
                                         new Vector2(0.5f, 0.5f), //new sprite origin \in [0,1]^2
@@ -20,16 +20,16 @@ public class AnglerFishBehaviour : Enemy
                                         0, //amount by which the sprite mesh should be expanded outwards
                                         SpriteMeshType.FullRect //mesh type
                                         );
-        initialSetup(100,//health 
-                        100,//max health 
-                        10,//damage
-                        10,//melee damage
-                        .5f,//melee cooldown
-                        0.5f,//speed
-                        "Angler fish enemy",//name 
+        initialSetup(afd.health,//health 
+                        afd.health,//max health 
+                        afd.damage,//damage
+                        afd.meleeDamage,//melee damage
+                        afd.meleeCooldown,//melee cooldown
+                        afd.chaseSpeed,//speed
+                        afd.gameObjectName,//name 
                         sprite,//sprite 
-                        0.5f,//sprite scale modifier
-                        0.0f//stopping distance
+                        afd.textureScale,//sprite scale modifier
+                        afd.stoppingDistance//stopping distance
                         );
     }
 
