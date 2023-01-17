a2ceeab073e13d0471e8a5c919eb7397d376a4cf
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-01-17 14:58:02 +0100

    some changes

--- Crabasite/Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Crabasite/Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -13,7 +13,6 @@ public class EnemyProjectile : MonoBehaviour
     public delegate void collision_delegate(Collider2D other);
     public collision_delegate del;
 
-<<<<<<<< HEAD:Crabasite/Assets/Scripts/Enemies/EnemyProjectile.cs
     public void Setup(GameObject _parent, 
                         GameObject _target, 
                         Sprite _sprite, 
@@ -21,12 +20,6 @@ public class EnemyProjectile : MonoBehaviour
                         float _speed,
                         collision_delegate _delegate
                         ){
-========
-    public void Setup(GameObject _parent, GameObject _target, Sprite _sprite){
-        msd = Resources.Load<MantisShrimpData>("Scriptable Objects/EnemyData/MantisShrimpData");
-
-        //set hierarchy and world position
->>>>>>>> save-and-load-system:Crabasite/Assets/Scripts/Enemies/MantisShrimpSpear.cs
         parent = _parent;
         speed = _speed;
         del = _delegate;
@@ -40,21 +33,11 @@ public class EnemyProjectile : MonoBehaviour
 
         //rotation
         float angle = 90 + Vector3.Angle(new Vector3(0, 1, 0), targetDirection);
-<<<<<<<< HEAD:Crabasite/Assets/Scripts/Enemies/EnemyProjectile.cs
         if(Mathf.Sign((gameObject.transform.position - _target.transform.position).x) == 1)//flip Spear correctly
             angle += 180;
         gameObject.transform.Rotate(new Vector3(0, 0, angle), Space.Self);
 
         gameObject.name = parent.name+" Projectile";
-========
-        gameObject.transform.Rotate(new Vector3(0, 0, angle));
-        if(Mathf.Sign((gameObject.transform.position - _target.transform.position).x) == 1) {//flip Spear correctly
-            gameObject.transform.Rotate(new Vector3(0, 0, 180));
-        }
-
-        //set gameobject properties
-        gameObject.name = "Mantis Shrimp Spear";
->>>>>>>> save-and-load-system:Crabasite/Assets/Scripts/Enemies/MantisShrimpSpear.cs
         gameObject.tag = "Enemy";
         gameObject.layer = LayerMask.NameToLayer("Raycast");
 
