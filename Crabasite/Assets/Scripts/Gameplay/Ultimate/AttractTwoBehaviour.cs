29e008c5fbbef49afae49a90d812d88de598c16f
Mavodeli <email@mavodeli.de>
2023-01-19 00:07:54 +0100

    AttractTwoBehaviour changes
    - fix of functionality
    - stop if collides along the way
    - bounce off collision

--- Crabasite/Assets/Scripts/Gameplay/Ultimate/AttractTwoBehaviour.cs
+++ Crabasite/Assets/Scripts/Gameplay/Ultimate/AttractTwoBehaviour.cs
@@ -10,6 +10,7 @@ public class AttractTwoBehaviour : MonoBehaviour
     public float speed;
     Rigidbody2D myRigidbody;
     float _force;
+    Vector3 gameObjectToTargetDirection;
 
     //Collision detection script
     CollisionDetector detector;
@@ -21,34 +22,39 @@ public class AttractTwoBehaviour : MonoBehaviour
     void Start()
     {
         //Initializes fields
-        detector = target.AddComponent<CollisionDetector>();
         myTransform = gameObject.GetComponent<Transform>();
         targetTransform = target.GetComponent<Transform>();
         myRigidbody = gameObject.GetComponent<Rigidbody2D>();
         _force = 5;
+
+        //set collision detection to continuous
+        myRigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if the Collision detector detects a collision with anything, the pull script will be destroyed to stop the pull
-        if (detector.isColliding)
-        {
-            if(gameObject.tag == "Collectable"){
-                gameObject.GetComponent<BoxCollider2D>().SendMessage("EnemyTakeDmg", 5*_force, SendMessageOptions.DontRequireReceiver);
-            }
-            if(gameObject.tag == "Enemy"){
-                gameObject.GetComponent<BoxCollider2D>().SendMessage("addHealth", -5*_force, SendMessageOptions.DontRequireReceiver);
-            }
-            Destroy(GetComponent<AttractTwoBehaviour>());
-        }
-
         //pulls the object towards the target
         //myTransform.position = Vector3.MoveTowards(myTransform.position, targetTransform.position, speed * Time.deltaTime);
-        Vector3 gameObjectToTargetDirection = new Vector3(target.transform.position.x - gameObject.transform.position.x, target.transform.position.y - gameObject.transform.position.y, 0);
+        gameObjectToTargetDirection = new Vector3(target.transform.position.x - gameObject.transform.position.x, target.transform.position.y - gameObject.transform.position.y, 0);
         gameObjectToTargetDirection.Normalize();
 
         myRigidbody.AddForce(gameObjectToTargetDirection * _force);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if the Collision detector detects a collision with anything, the pull script will be destroyed to stop the pull
+        if(gameObject.tag == "Collectable"){
+            gameObject.GetComponent<Collider2D>().SendMessage("EnemyTakeDmg", 5*_force, SendMessageOptions.DontRequireReceiver);
+        }
+        if(gameObject.tag == "Enemy"){
+            gameObject.GetComponent<Collider2D>().SendMessage("addHealth", -5*_force, SendMessageOptions.DontRequireReceiver);
+        }
+        //return collision detection to discrete
+        myRigidbody.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
+        // bounce off collision
+        myRigidbody.AddForce(-gameObjectToTargetDirection * 4 * _force);
+        Destroy(this);
+    }
 }
