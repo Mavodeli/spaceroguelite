a42053264b917bf6d68f148661cc055053232cd1
ElectricUnit <apatonole@gmail.com>
2023-01-18 21:33:53 +0100

    added player prefab
    added assembly def for crab claw script

--- Crabasite/Assets/Scripts/Gameplay/Core/CrabClaw.cs
+++ Crabasite/Assets/Scripts/Gameplay/Core/CrabClaw.cs
@@ -16,9 +16,6 @@ public class CrabClaw : MonoBehaviour
     //the Layer in which the ray checks for intersections (default: 'Enemies')
     private LayerMask DetectionLayer;
 
-    // inventory to check if active
-    [SerializeField] private Transform inventory;
-
     Rigidbody2D objectRigidbody;
 
     void Awake(){
@@ -46,7 +43,8 @@ public class CrabClaw : MonoBehaviour
         //- direction: normalized Mouse Cursor Position
         //- maximal t: +infinity
         RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePos_relative_to_player, Mathf.Infinity, DetectionLayer);
-        if(hit.collider != null && !inventory.gameObject.activeSelf){ // check if inventory is off to enable crab claw 
+        GameObject inventory = GameObject.FindGameObjectWithTag("Inventory");
+        if(hit.collider != null && !inventory.GetComponent<InventoryManager>().inventoryIsOpened){ // check if inventory is off to enable crab claw 
             //playerPos_relative_to_hit: vector that points [location where the ray hits a collider] -> Player
             Vector2 playerPos_relative_to_hit = transform.position-hit.transform.position;
             playerPos_relative_to_hit.Normalize();
