ec9f6c6532578634dbbb3048cd48e389d34aa471
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-01-22 13:24:03 +0100

    updated prog script template

--- Crabasite/Assets/Scripts/ProgressionSystem/SpaceLevel_ProgressionScript.cs
+++ Crabasite/Assets/Scripts/ProgressionSystem/SpaceLevel_ProgressionScript.cs
@@ -2,6 +2,7 @@ using Codice.Client.BaseCommands;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class SpaceLevel_ProgressionScript : ProgressionParentClass
@@ -12,8 +13,30 @@ public class SpaceLevel_ProgressionScript : ProgressionParentClass
 
     private void Awake(){
         player = GameObject.FindGameObjectWithTag("Player");
+
+        //TODO: workaround until GameData resetting is implemented!
+        PT.setFlag("triggeredEnemySpawner", false);
+
         triggerMap.Add("PH black hole", delegate () {
+            //kill player upon entering the black hole
             player.SendMessage("addHealth", -Mathf.Infinity, SendMessageOptions.DontRequireReceiver);
+        });
+        triggerMap.Add("TriggerSpaceshipEntrance", delegate () {
+            SceneManager.LoadScene("SampleScene");//TODO: set correct scene ;)
+            Time.timeScale = 1;
+            //TODO: show button instead of instant teleport!!!
+        });
+        triggerMap.Add("TriggerElectroAsteroidsEnemySpawner", delegate () {
+            if(!PT.getFlag("triggeredEnemySpawner")){
+                int count = 3;
+                for(int i = 0; i < count; i++){
+                    GameObject fish = Instantiate(Resources.Load<GameObject>("Prefabs/Enemy"));
+                    fish.AddComponent<PufferFishBehaviour>();
+                    Vector3 spawnOffset = new Vector3(0, 0, 0);
+                    fish.transform.position = player.transform.position + spawnOffset;
+                }
+                PT.setFlag("triggeredEnemySpawner");
+            }
         });
     }
 
--- Crabasite/Assets/Scripts/ProgressionSystem/Template_ProgressionScript.cs
+++ Crabasite/Assets/Scripts/ProgressionSystem/Template_ProgressionScript.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class Template_ProgressionScript : ProgressionParentClass
 {
     private Dictionary<string, OnTriggerEnterDelegate> triggerMap = new Dictionary<string, OnTriggerEnterDelegate>();
+    private GameObject player;
 
 
     private void Awake(){
+        //save player GameObject for easy access
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //1st parameter: the name of the GameObject (the 'trigger' object with the ProgressionTrigger script)
+        //2nd parameter: the function that should be executed OnTriggerEnter
         triggerMap.Add("BobTheTrigger", delegate () {
             //TODO
         });
