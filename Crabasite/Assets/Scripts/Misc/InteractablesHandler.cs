4858802532ba18e9c887998d6d86a35979fcdc98
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-02-11 15:13:08 +0100

    Update InteractablesHandler.cs
    commented out duplicate code

--- Crabasite/Assets/Scripts/Misc/InteractablesHandler.cs
+++ Crabasite/Assets/Scripts/Misc/InteractablesHandler.cs
@@ -18,36 +18,36 @@ public class InteractablesHandler : MonoBehaviour
 
     void Update()
     {
-        //check if all interactables have the button script, if not, add missing scripts
-        foreach(GameObject interactable in GameObject.FindGameObjectsWithTag("Interactable")){
-            InteractionButton script = interactable.GetComponent<InteractionButton>();
-            if(script == null){
-                script = interactable.AddComponent<InteractionButton>();
+    //     //check if all interactables have the button script, if not, add missing scripts
+    //     foreach(GameObject interactable in GameObject.FindGameObjectsWithTag("Interactable")){
+    //         InteractionButton script = interactable.GetComponent<InteractionButton>();
+    //         if(script == null){
+    //             script = interactable.AddComponent<InteractionButton>();
 
-                if(interactable.name == "SpaceshipEntrance"){
-                    script.Setup(delegate () {
-                        spaceShipDoor.Play(); // TODO Loading the scene right after this statement causes the sound to not play/interuppt
-                        SceneManager.LoadScene("SampleScene");//TODO: set correct scene ;)
-                        Time.timeScale = 1;
-                    }, "e");
-                    script.setNewOffset(new Vector3(0, 0, 0));
-                }
+    //             if(interactable.name == "SpaceshipEntrance"){
+    //                 script.Setup(delegate () {
+    //                     spaceShipDoor.Play(); // TODO Loading the scene right after this statement causes the sound to not play/interuppt
+    //                     SceneManager.LoadScene("SampleScene");//TODO: set correct scene ;)
+    //                     Time.timeScale = 1;
+    //                 }, "e");
+    //                 script.setNewOffset(new Vector3(0, 0, 0));
+    //             }
 
-                if(interactable.name == "blue blobby mass circle"){
-                    script.Setup(delegate () {
-                        int ult = 0;//attract two
-                        GameObject player = GameObject.FindGameObjectWithTag("Player");
-                        player.SendMessage("SwitchUltimate", ult, SendMessageOptions.DontRequireReceiver);
-                        GameObject IM = GameObject.FindGameObjectWithTag("Inventory");
-                        IM.SendMessage("unlockUltimate", ult, SendMessageOptions.DontRequireReceiver);//TODO: not implemented yet!!!
-                        collectUltimate.Play();
-                        Destroy(GameObject.Find(interactable.name));//kinda tricky to get the delegate to destroy the correct gameObject ;)
-                    }, "e");
-                    script.setNewOffset(new Vector3(0, 0, 0));
-                }
+    //             if(interactable.name == "blue blobby mass circle"){
+    //                 script.Setup(delegate () {
+    //                     int ult = 0;//attract two
+    //                     GameObject player = GameObject.FindGameObjectWithTag("Player");
+    //                     player.SendMessage("SwitchUltimate", ult, SendMessageOptions.DontRequireReceiver);
+    //                     GameObject IM = GameObject.FindGameObjectWithTag("Inventory");
+    //                     IM.SendMessage("unlockUltimate", ult, SendMessageOptions.DontRequireReceiver);//TODO: not implemented yet!!!
+    //                     collectUltimate.Play();
+    //                     Destroy(GameObject.Find(interactable.name));//kinda tricky to get the delegate to destroy the correct gameObject ;)
+    //                 }, "e");
+    //                 script.setNewOffset(new Vector3(0, 0, 0));
+    //             }
 
-                //add more interactables here simply by using if checks
-            }
-        }
+    //             //add more interactables here simply by using if checks
+    //         }
+    //     }
     }
 }
