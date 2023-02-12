993e77ac60ef463aade47d1f2c0b703451c7f90d
ElectricUnit <apatonole@gmail.com>
2023-02-13 00:50:23 +0100

    added new sprites for mana bar and healthbar for player stats

--- Crabasite/Assets/Scripts/Gameplay/HealthSystem/HealthBar.cs
+++ Crabasite/Assets/Scripts/Gameplay/HealthSystem/HealthBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
@@ -14,5 +15,9 @@ public class HealthBar : MonoBehaviour
     // event for changing healtbarsize
     private void HealthSystem_OnHealthChanged(object sender, System.EventArgs e){
         transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(),1);
+        if (transform.parent.tag == "HUD")
+        {
+            GameObject.FindGameObjectWithTag("HUD").transform.Find("PlayerHealthBar").GetComponent<Slider>().value = healthSystem.GetHealthPercent();
+        }
     }
 }
