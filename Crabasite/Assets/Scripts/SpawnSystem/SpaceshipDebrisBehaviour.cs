4691d2499944ac48597fd0462516c6658f57f335
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-03-11 15:46:14 +0100

    integrated spaceship parts (#84)

---
+++ Crabasite/Assets/Scripts/SpawnSystem/SpaceshipDebrisBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipDebrisBehaviour : PhysicalEntity
+{
+    private AudioSource pickupSound;
+    private InteractionButton ib;
+
+    public void Start(){
+
+        //setup sound
+        pickupSound = (AudioSource) (GameObject.Find("PickupObject")).GetComponent(typeof (AudioSource));
+
+        //setup Pickup (formerly done by the PickupHandler)
+        ib = gameObject.AddComponent<InteractionButton>();
+        ib.Setup(delegate () {
+            pickupSound.Play();
+
+            if(!GameObject.FindGameObjectWithTag("Inventory").GetComponent<InventoryManager>().inventoryIsOpened){
+                InventoryManager.Instance.AddItem("SpaceshipDebris");
+                Destroy(gameObject);
+            }
+        }, "e");
+        ib.setNewOffset(new Vector3(0, 0, 0));
+    }
+}
