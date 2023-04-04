2f2f9f8e5ef4aa59da92a13035333156f2063e6e
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-04-04 16:54:59 +0200

    Credits! (mechanic for #67)
    scaling should properly adapt to screen size?

--- Crabasite/Assets/Scripts/StoryScenes/ButtonBehaviour.cs
+++ Crabasite/Assets/Scripts/StoryScenes/ButtonBehaviour.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class ButtonBehaviour : MonoBehaviour
 {
     int counter = 0;
+    private string postEndingScene = "Credits";//"MainMenu";
 
     public void LoadStoryScene1()
     {
@@ -43,12 +44,12 @@ public class ButtonBehaviour : MonoBehaviour
         if (counter == 0)
         {
             TMP_Text text = GameObject.Find("EndingText").GetComponent<TMP_Text>();
-            text.text = "In accordance with the Imperial Criminal Justice Declaration and the Criminal Prosecution Regulations for Independent Contractors in Service of Imperial Frontier Colonies you have been found guilty and are therefore convicted of (1) theft of imperial property, (2) insufficient fulfillment of contract obligations towards imperial authorities and (3) murder in 1257 cases. All the offenses contained herein of which you were convicted have been added to your criminal record. You currently have at least one conviction or pending charge under imperial law that is not either sealed or expunged, therefore your status as an Imperial Citizen is hereby revoked with immediate effect. Since you are convicted of at least one criminal offense against His Imperial Highness, an arrest warrant as well as a bounty have been issued against you in all systems under expanded imperial juristiction. Since you no longer possess the status of Imperial Citizen, please return any and all items that are considered state property under imperial law in your possession. Failing to do so will result in a charge against you, the amount of which will be determined based on any and all such items in your possession at the time your citizenship was revoked. Please report to your Legal Supervisor to clear your outstanding charges and/or receive punishment for your convictions.";
+            text.text = "'In accordance with the Imperial Criminal Justice Declaration and the Criminal Prosecution Regulations for Independent Contractors in Service of Imperial Frontier Colonies you have been found guilty and are therefore convicted of (1) theft of imperial property, (2) insufficient fulfillment of contract obligations towards imperial authorities and (3) murder in 1257 cases. All the offenses contained herein of which you were convicted have been added to your criminal record. You currently have at least one conviction or pending charge under imperial law that is not either sealed or expunged, therefore your status as an Imperial Citizen is hereby revoked with immediate effect. Since you are convicted of at least one criminal offense against His Imperial Highness, an arrest warrant as well as a bounty have been issued against you in all systems under expanded imperial juristiction. Since you no longer possess the status of Imperial Citizen, please return any and all items that are considered state property under imperial law in your possession. Failing to do so will result in a charge against you, the amount of which will be determined based on any and all such items in your possession at the time your citizenship was revoked. Please report to your Legal Supervisor to clear your outstanding charges and/or receive punishment for your convictions.'";
             counter += 1;
         }
         else if (counter >= 1)
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(postEndingScene);
         }
     }
 
@@ -57,12 +58,12 @@ public class ButtonBehaviour : MonoBehaviour
         if (counter == 0)
         {
             TMP_Text text = GameObject.Find("EndingText").GetComponent<TMP_Text>();
-            text.text = "For your service to His Imperial Highness in retrieving a cure for the parasite infection that plaques the imperial colony on Cryke 2H23, the Ministry of Medical Affairs has been granted permission and is most pleased to bestow the title of 'Honorary Colonial Subject' upon you. You have furthermore been granted permission to purchase residential property in the Imperial City. To claim the certificate signed by the High Secretary herself and the monetary compensation that come with your new civil status, please visit your Legal Supervisor at your convenience. Sincerely, Qarea, Chief Scientist on Cryke 2H23 on behalf of the Ministry of Medical Affairs. PS: I'm very sorry that on account of your physical condition we cannot meet in person and I hope that this message finds you in a state where you are still able to comprehend its meaning. Your wife passed away half an hour ago. The treatment sadly didn't do much to contain the parasite, the infection had already spread too far by the time you returned with the cure. According to her wishes, we will scatter her cremated remains outside of Cryke's atmosphere, as we did with those of your child. Since you do not have any relative eligible to inherit your possessions, they will be assimilated into colonial property upon your passing.";
+            text.text = "'For your service to His Imperial Highness in retrieving a cure for the parasite infection that plaques the imperial colony on Cryke 2H23, the Ministry of Medical Affairs has been granted permission and is most pleased to bestow the title of 'Honorary Colonial Subject' upon you. You have furthermore been granted permission to purchase residential property in the Imperial City. To claim the certificate signed by the High Secretary herself and the monetary compensation that come with your new civil status, please visit your Legal Supervisor at your convenience. Sincerely, Qarea, Chief Scientist on Cryke 2H23 on behalf of the Ministry of Medical Affairs. PS: I'm very sorry that on account of your physical condition we cannot meet in person and I hope that this message finds you in a state where you are still able to comprehend its meaning. Your wife passed away half an hour ago. The treatment sadly didn't do much to contain the parasite, the infection had already spread too far by the time you returned with the cure. According to her wishes, we will scatter her cremated remains outside of Cryke's atmosphere, as we did with those of your child. Since you do not have any relative eligible to inherit your possessions, they will be assimilated into colonial property upon your passing.'";
             counter += 1;
         }
         else if (counter >= 1)
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(postEndingScene);
         }
     }   
 
@@ -71,12 +72,12 @@ public class ButtonBehaviour : MonoBehaviour
         if (counter == 0)
         {
             TMP_Text text = GameObject.Find("EndingText").GetComponent<TMP_Text>();
-            text.text = "Hey there! I just wanted to let you know that the medication they developed from the cure sample you brought back from the Asclepius is working great. Sasha responded really well to the treatment, they were among the first to receive a dose. By now they are as good as new and I'm on the mend as well. They are asking about you all the time. Yeah, I haven't told them yet. I know I shouldn't put it off like that, I... guess I still haven't completely come to terms with it myself. It feels so unreal y'know? Like, I always imagined the three of us would get through all of this together. That we would live our lives together. I suppose I haven't fully realised that... yeah, that that's it. You're gone, You have moved on without me. And the last thing you did was to save all of us... I'm not even sure I deserve that kind of sacrifice. Anyway, I don't want to end this message on such a heavy note :) I hope you're doing well, where ever you are right now. I hope this other place is nice and that you are happy. I think I should wrap it up for now so... if you can and if it's not too much to ask, wait for me. I wish for us to be reunited again someday, I really do. And if only to tell you about all the things happened in your absence, about how Sasha grew up, how they did in school, about the first partner they brought home and so on. But for now... so long!";
+            text.text = "'Hey there! I just wanted to let you know that the medication they developed from the cure sample you brought back from the Asclepius is working great. Sasha responded really well to the treatment, they were among the first to receive a dose. By now they are as good as new and I'm on the mend as well. They are asking about you all the time. Yeah, I haven't told them yet. I know I shouldn't put it off like that, I... guess I still haven't completely come to terms with it myself. It feels so unreal y'know? Like, I always imagined the three of us would get through all of this together. That we would live our lives together. I suppose I haven't fully realised that... yeah, that that's it. You're gone, You have moved on without me. And the last thing you did was to save all of us... I'm not even sure I deserve that kind of sacrifice. Anyway, I don't want to end this message on such a heavy note :) I hope you're doing well, where ever you are right now. I hope this other place is nice and that you are happy. I think I should wrap it up for now so... if you can and if it's not too much to ask, wait for me. I wish for us to be reunited again someday, I really do. And if only to tell you about all the things happened in your absence, about how Sasha grew up, how they did in school, about the first partner they brought home and so on. But for now... so long!'";
             counter += 1;
         } else if (counter >= 1)
         {
             counter = 0;
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(postEndingScene);
         }
     }
 }
---
+++ Crabasite/Assets/Scripts/Utilities/CreditsConstructor.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class CreditsConstructor : MonoBehaviour
+{
+    private GameObject parent;
+    [SerializeField] float scroll_speed = .1f;
+    private float break_bound;
+    private float between_tiles;//spacing
+    private float all_tiles_height = 0;
+    private bool tiles_height_was_updated = false;
+    private TimerObject delay_timer;
+    private float delay_time = 4;
+
+    void Start()
+    {
+        parent = gameObject;
+
+        //set scaling parameter according to screen size
+        between_tiles = 50;
+        break_bound = Screen.height+between_tiles*transform.childCount;
+
+        gameObject.GetComponent<VerticalLayoutGroup>().spacing = between_tiles;
+
+        string path = "Assets/Resources/credits.txt";
+        if(File.Exists(path))
+        {
+            try
+            {
+                FileStream stream = new FileStream(path, FileMode.Open);
+                StreamReader reader = new StreamReader(stream);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    GameObject newTile = TMPTile(line);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Error occured when trying to load credits data from file:" + path + "\n" + e);
+            }
+        }
+
+        delay_timer = new TimerObject(gameObject.name+" delay_timer");
+        delay_timer.start(delay_time);
+
+        StartCoroutine(waitForCanvasDraw());
+    }
+
+    void FixedUpdate()
+    {
+        //update tiles height    
+        if(!tiles_height_was_updated && all_tiles_height != 0){
+            break_bound += all_tiles_height;
+            tiles_height_was_updated = true;
+        }
+
+        if(delay_timer.runs()) return;
+    
+        if(parent.GetComponent<RectTransform>().position.y <= break_bound)
+            parent.GetComponent<RectTransform>().position += new Vector3(0, scroll_speed, 0);
+        else{
+            delay_timer.setOnRunningOut(delegate(){SceneManager.LoadScene("MainMenu");});
+            delay_timer.start(delay_time);
+        }
+    }
+
+    private GameObject TMPTile(string content){
+        GameObject go = new GameObject();
+        go.transform.parent = parent.transform;
+        go.name = "TMPTile";
+        try{
+            TMP_Text textComp = go.AddComponent<TextMeshProUGUI>();
+            tileFormatter(textComp, content);
+        }
+        catch(System.ArgumentOutOfRangeException){//spacing tile
+            go.name = "spacing "+go.name;
+        }
+        return go;
+    }
+
+    private void tileFormatter(TMP_Text tile, string content){
+        //options set for all
+        tile.color = new Color(1, 1, 1, .85f);
+        tile.alignment = TextAlignmentOptions.Center;
+
+        string format = content.Substring(0, 1);
+        tile.text = content.Substring(2);
+
+        if(format == "+"){//main header ('Credits')
+            tile.fontSize = (40*Screen.height)/1080;
+            tile.fontStyle = FontStyles.Bold;
+            parent.GetComponent<RectTransform>().position -= new Vector3(0, tile.fontSize, 0);//dirty fix for main header being visible during start delay
+        }
+        else if(format == "#"){//header (job)
+            tile.fontSize = (34*Screen.height)/1080;
+            tile.fontStyle = FontStyles.Bold;
+        }
+        else if(format == "-"){//body (name)
+            tile.fontSize = (26*Screen.height)/1080;
+        }
+        else{
+            throw new System.NotSupportedException("The formatting prefix "+format+" is not a supported line prefix for a credits file! Currently only #, + and - are supported.");
+        }
+    }
+
+    private IEnumerator waitForCanvasDraw(){
+        yield return new WaitForEndOfFrame();
+        all_tiles_height = (-1)*transform.GetChild(transform.childCount-1).GetComponent<RectTransform>().position.y;
+        all_tiles_height += transform.GetChild(transform.childCount-1).GetComponent<TextMeshProUGUI>().fontSize;
+    }
+}
