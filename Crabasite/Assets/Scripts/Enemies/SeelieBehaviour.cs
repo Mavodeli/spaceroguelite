ede6cec1951871a14b649eed4f36bf4c35cdbf5c
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-04-07 13:14:48 +0200

    adjusted AS level grid, for real this time...

--- Crabasite/Assets/Scripts/Enemies/SeelieBehaviour.cs
+++ Crabasite/Assets/Scripts/Enemies/SeelieBehaviour.cs
@@ -15,7 +15,6 @@ public class SeelieBehaviour : MonoBehaviour
     private Path path;
     private int currentWaypoint;
     private float nextWaypointDistance;
-    [SerializeField] private float stoppingDistance;
     private GameObject player;
     [SerializeField] private Vector3 destination;
 
@@ -30,6 +29,9 @@ public class SeelieBehaviour : MonoBehaviour
         currentWaypoint = 0;
         nextWaypointDistance = 3.0f;
         player = GameObject.FindGameObjectWithTag("Player");
+
+        destination = GameObject.Find("TheCure").transform.position;
+
         StartCoroutine(UpdateCoroutine(.5f));
     }
 
@@ -54,31 +56,51 @@ public class SeelieBehaviour : MonoBehaviour
 
     void FixedUpdate()
     {
-        //movement
-        Vector3 force, direction;
+        //init
+        Vector3 force, direction = Vector3.zero;
+
+        Debug.Log(Vector3.Distance(gameObject.transform.position, destination));
+
+        //move when near player
         if (Vector3.Distance(gameObject.transform.position, player.transform.position) <= maxDistanceToPlayer)
         {
             if (path == null) return;
             if (currentWaypoint >= path.vectorPath.Count) return;
+
+            //dont move if destination reached
+            if(Vector3.Distance(gameObject.transform.position, destination) <= maxDistanceToPlayer){
+                //fix overshooting destination
+                haltMovement(direction);
+                return;
+            }
+
+            //actual movement
             direction = path.vectorPath[currentWaypoint] - gameObject.transform.position;
             direction.Normalize();
             if (Vector2.Distance(gameObject.transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
                 currentWaypoint++;
-            
-            if(Vector3.Distance(gameObject.transform.position, destination) <= maxDistanceToPlayer)
-                haltMovement();
         }
-        else
+        else//dont move when not near player
         {
-            haltMovement();
-            direction = Vector3.zero;
+            haltMovement(direction);
+            return;
         }
+
+        // randomizeDirection(direction, 3);
         force = direction * speed;
+        // Debug.Log(force);
         rb.AddForce(force);
     }
 
-    private void haltMovement(){
+    private void haltMovement(Vector3 direction){
         rb.velocity = new Vector2(0, 0);
         rb.angularVelocity = 0.1f;
+        direction = Vector3.zero;
+    }
+
+    private void randomizeDirection(Vector3 direction, float factor){
+        float rndX = Random.Range(-1, 1);
+        float rndY = Random.Range(-1, 1);
+        direction += new Vector3(rndX, rndY, 0)*factor;
     }
 }
